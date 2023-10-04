28a60657968ed61722b5b55b303cbaf1aeea49f1
Loduan10 <loyee1107@gmail.com>
2023-10-04 12:16:57 -0700

    Second Push

--- asl_alphabet_detection.py
+++ asl_alphabet_detection.py
@@ -6,5 +6,23 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 #accessing data sets
+
 df = pd.read_csv('/content/sign_mnist_train.csv')
 df.head()
+def load_data(path):
+	df = pd.read_csv(path)
+	y = np.array([label if label < 9
+				else label-1 for label in df['label']])
+	df = df.drop('label', axis=1)
+	x = np.array([df.iloc[i].to_numpy().reshape((28, 28))
+				for i in range(len(df))]).astype(float)
+	x = np.expand_dims(x, axis=3)
+	y = pd.get_dummies(y).values
+
+	return x, y
+
+X_train, Y_train = load_data('/content/sign_mnist_train.csv')
+X_test, Y_test = load_data('/content/sign_mnist_test.csv')
+
+print(X_train.shape, Y_train.shape)
+print(X_test.shape, Y_test.shape)
--- camera_software_import.py
+++ camera_software_import.py
@@ -1,6 +1,7 @@
 #importing libraries and downloading mediapipe and open cv
 import cv2
 import mediapipe as mp
+import numpy as np
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 model_path = '/absolute/path/to/gesture_recognizer.task'
